23c38619a2be7908754c179dae1fff4929b40faa
Sebastien Stordeur <stordeursebastien0@gmail.com>
2022-09-27 14:20:24 +0200

    update readme

--- README.md
+++ README.md
@@ -9,14 +9,6 @@ npm install --save selectlist
 ## Usage
 
 ```tsx
-Props: {
-  data: string[] = ["abc", "def"]
-  headline: string | number = "Something"
-  class: string = "personalized class"
-  listStyle: string = "another class"
-  activeValueClass: string = "another class"
-}
-
 <ListSelect data={array} headline={headline} />
 ```
 
@@ -28,11 +20,12 @@ Props: {
   | headline         | true     | string|number |
   | class            | false    | string        |
   | listStyle        | false    | string        |
-  | activeValueClass | false    | string        | 
+  | activeValueStyle | false    | string        | 
   ```
 
-class && labelStyle && activeValueClass are just different applied classes
+class, labelStyle, activeValueStyle are just different applied classes
 
 ## License
 
 MIT © [SebastienStordeur](https://github.com/SebastienStordeur)
+
--- dist/index.modern.js
+++ dist/index.modern.js
@@ -1,36 +1,45 @@
 import React, { useState } from 'react';
 
-const Option = props => {
+var Option = function Option(props) {
   return React.createElement("li", {
     value: props.value,
-    className: `${props.value === props.activeValue ? props.activeValueStyle : props.listStyle}`,
-    onClick: () => props.onClick(props.value)
+    className: "" + (props.value === props.activeValue ? props.activeValueStyle : props.listStyle),
+    onClick: function onClick() {
+      return props.onClick(props.value);
+    }
   }, props.value);
 };
 
-const ListSelect = props => {
+var ListSelect = function ListSelect(props) {
   var _props$data;
 
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [activeValue, setActiveValue] = useState('');
+  var _useState = useState(false),
+      isMenuOpen = _useState[0],
+      setIsMenuOpen = _useState[1];
 
-  const openMenuHandler = () => {
-    setIsMenuOpen(prevValue => !prevValue);
+  var _useState2 = useState(''),
+      activeValue = _useState2[0],
+      setActiveValue = _useState2[1];
+
+  var openMenuHandler = function openMenuHandler() {
+    setIsMenuOpen(function (prevValue) {
+      return !prevValue;
+    });
   };
 
-  const setActiveValueHandler = value => {
+  var setActiveValueHandler = function setActiveValueHandler(value) {
     setActiveValue(value);
   };
 
   return React.createElement("div", {
     id: 'select-wrapper',
-    className: props.class
+    className: props["class"]
   }, React.createElement("div", null, React.createElement("ul", {
     className: 'h-8 py-1 px-4',
     onClick: openMenuHandler
   }, React.createElement("label", null, activeValue ? activeValue : props.headline), React.createElement("div", {
     className: 'absolute w-full left-0 mt-1 rounded-lg'
-  }, isMenuOpen && ((_props$data = props.data) === null || _props$data === void 0 ? void 0 : _props$data.map(value => {
+  }, isMenuOpen && ((_props$data = props.data) === null || _props$data === void 0 ? void 0 : _props$data.map(function (value) {
     return React.createElement(Option, {
       value: value,
       key: Math.random().toString(),
